d5792d08346ad11525414531986f24b4b012f9c4
Kilimanjaro Jan <kilimanjaro.jan@gmail.com>
2019-02-17 15:05:09 +0900

    race-summary-record.ts を追加した

---
+++ src/app/record/race-summary-record.ts
@@ -0,0 +1,15 @@
+export class RaceSummaryRecord {
+  kaisaiCd: string;
+  kaisaiNm: string;
+  raceNo: number;
+  umaCnt: number;
+  tnpkOddsTime: string;
+  umrnOddsTime: string;
+  tan10Odds: number;
+  umrn1Odds: number;
+  fuku8Odds: number;
+  tanAnaFlg: boolean;
+  umrnAnaFlg: boolean;
+  fukuAnaFlg: boolean;
+  jdgeRslt: string;
+}
