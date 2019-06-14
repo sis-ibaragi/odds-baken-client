36dfe3dee6b9eb9362f9e2f58a7a0e0817b80255
Kilimanjaro Jan <kilimanjaro.jan@gmail.com>
2019-06-14 17:53:33 +0900

    各データを localhost:3000 の Web API 呼び出しにより取得するように実装した

--- src/app/service/race-odds.service.ts
+++ src/app/service/race-odds.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs/operators';
+
 import { UmrnOddsRecord } from '../record/umrn-odds-record';
 import { TanOddsRecord } from '../record/tan-odds-record';
 import { FukuOddsRecord } from '../record/fuku-odds-record';
@@ -18,36 +21,81 @@ import { kaisaiMap } from './mock/mock-kaisai-map';
   providedIn: 'root',
 })
 export class RaceOddsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   getKaisaiInfo(kaisaiCd: string): Promise<KaisaiRecord> {
-    return Promise.resolve(kaisaiMap[kaisaiCd]);
+    // return Promise.resolve(kaisaiMap[kaisaiCd]);
+    return this.http
+      .get(`http://localhost:3000/kaisai/${kaisaiCd}`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as KaisaiRecord)
+      .catch(this.handleError);
   }
 
   getOddsTimeList(kaisaiCd: string, raceNo: number): Promise<OddsTimeRecord[]> {
-    return Promise.resolve(oddsTimeList);
+    // return Promise.resolve(oddsTimeList);
+    return this.http
+      .get(`http://localhost:3000/odds/${kaisaiCd}/${raceNo}/odds-times`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as OddsTimeRecord[])
+      .catch(this.handleError);
   }
 
   getUmrnOddsList(kaisaiCd: string, raceNo: number, oddsTimeNo: number): Promise<UmrnOddsRecord[]> {
-    return Promise.resolve(umrnOddsList);
+    // return Promise.resolve(umrnOddsList);
+    return this.http
+      .get(`http://localhost:3000/odds/${kaisaiCd}/${raceNo}/${oddsTimeNo}/umrn`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as UmrnOddsRecord[])
+      .catch(this.handleError);
   }
 
   getTanOddsList(kaisaiCd: string, raceNo: number, oddsTimeNo: number): Promise<TanOddsRecord[]> {
-    return Promise.resolve(tanOddsList);
+    // return Promise.resolve(tanOddsList);
+    return this.http
+      .get(`http://localhost:3000/odds/${kaisaiCd}/${raceNo}/${oddsTimeNo}/tan`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as TanOddsRecord[])
+      .catch(this.handleError);
   }
 
   getFukuOddsList(kaisaiCd: string, raceNo: number, oddsTimeNo: number): Promise<FukuOddsRecord[]> {
-    return Promise.resolve(fukuOddsList);
+    // return Promise.resolve(fukuOddsList);
+    return this.http
+      .get(`http://localhost:3000/odds/${kaisaiCd}/${raceNo}/${oddsTimeNo}/fuku`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as TanOddsRecord[])
+      .catch(this.handleError);
   }
 
-  getTnpkOddsDiffList(kaisaiCd: string, raceNo: number, oddsTimeNo: number): Promise<TnpkOddsDiffRecord[]> {
-    return Promise.resolve(tnpkOddsDiffList);
+  getTnpkOddsDiffList(kaisaiCd: string, raceNo: number, oddsTimeNo: number): Promise<Map<number, TnpkOddsDiffRecord>> {
+    // return Promise.resolve(tnpkOddsDiffList);
+    return this.http
+      .get(`http://localhost:3000/odds/${kaisaiCd}/${raceNo}/${oddsTimeNo}/diff`)
+      .pipe(first())
+      .toPromise()
+      .then(data => data as TnpkOddsDiffRecord[])
+      .then(list => {
+        const map = new Map<number, TnpkOddsDiffRecord>();
+        list.forEach(element => {
+          map.set(element.umaNo, element);
+        });
+        return map;
+      })
+      .catch(this.handleError);
   }
 
-  getOddsDiffTime(oddsTimeNo: number): Promise<OddsTimeRecord> {
-    return Promise.resolve(oddsTimeMap[oddsTimeNo]);
-  }
   getMarkOptions(): Promise<ValueLabelRecord[]> {
     return Promise.resolve(markOptions);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Error was occurred...', error);
+    return Promise.reject(error);
   }
 }
